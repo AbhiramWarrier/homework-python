c5d311420d7164ea225f7e91a83f642df1d73b52
AbhiramWarrier <kitkatmit1@gmail.com>
2025-08-24 20:00:44 +0200

    Add files via upload

---
+++ chatbot argument project .py
@@ -0,0 +1,10 @@
+def greet (name):
+    print("Hello", name)
+
+greet("John")    
+user_input =input("Do you want to know the weather today?")
+if user_input == "yes":           
+ print ("Today is a sunny day")
+else:
+   print("Ok. Have a great day John")
+
