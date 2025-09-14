6eaf55ab0ffe7fae72b3591b7a74ce4af7000484
AbhiramWarrier <kitkatmit1@gmail.com>
2025-09-14 23:08:18 +0200

    Add files via upload

---
+++ tuple.word guesser.py
@@ -0,0 +1,69 @@
+word = "happy"
+L = "letter"
+g = 0
+
+alphabet =("a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t.u,v,w,x,y,z")
+user = input("guess a letter in order to complete the word: ")
+
+tuplex = ["h","a","p","p","y"]
+for  i in range(0,5):
+
+    if user == tuplex[i]:
+        g = 1
+
+if g == 1:
+ print("you have guessed correctly")
+ 
+
+else:
+ print("incorrect")
+
+user = input("guess the next letter: ")
+if g == 1:
+   
+ print("you have guessed correctly")
+else:
+  print("incorrect")
+user = input("guess the next letter: ")
+if g == 1:
+ print("you guessed correctly")
+else:
+  print("incorrect")
+
+
+  if g == 1:
+   
+   print("you have guessed correctly")
+  else:
+
+   print("incorrect")
+user = input("guess the next letter:" )
+
+if g == 1:
+   
+ print("you have guessed correctly")
+else:
+  print("incorrect")
+
+
+if g == 1:
+   
+ print("you have guessed correctly")
+else:
+  print("incorrect")
+user = input("guess the next letter: ")
+print("you guessed the correct word")
+
+             
+
+  
+  
+
+
+
+
+ 
+
+
+
+
