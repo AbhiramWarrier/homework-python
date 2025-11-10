a535a63127a71097049b7c8d5ae6698fdae051a5
AbhiramWarrier <kitkatmit1@gmail.com>
2025-11-10 20:04:23 +0100

    Add files via upload

--- moving_game.py
+++ moving_game.py
@@ -20,7 +20,7 @@ class Sprite(pygame.sprite.Sprite):
    def __init__(self,color, height, width):
       super().__init__()
 
-      self.image = pygame.Surfase([width, height])
+      self.image = pygame.Surface([width, height])
       self.image.fill(color)
 
       self.rect = self.image.get_rect()
